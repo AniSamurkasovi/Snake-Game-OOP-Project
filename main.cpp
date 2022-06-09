2fccd5aecab3dcbb2946603e56e789b4d5a731fc
lixi111 <107041420+lixi111@users.noreply.github.com>
2022-06-09 18:38:20 +0400

    add printing walls
    I added function for setting and hiding cursor, setting color and printing walls

--- main.cpp
+++ main.cpp
@@ -55,3 +55,64 @@ namespace consolebackground//for background colour,  colours made using 4 attrib
 vector<int> snake;					//snake vector, to store position of snake in numbers
 bool food_eaten=1,colour=1,kids=0,self_hit=1,reverse_snake=0;//for settings
 int width=27,lenght=118,food_x=-1,food_y=-2,hsc,time1=20; //hsc- high score
+
+
+void hidecursor()//to hide the cursor in console window
+{
+   HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+   CONSOLE_CURSOR_INFO info;
+   info.dwSize = 100;
+   info.bVisible = FALSE;		//can be made TRUE to show if want
+   SetConsoleCursorInfo(consoleHandle, &info);
+}
+void setConsoleColour(unsigned short colour)//function to set colour
+{    // this is a function to print space with color
+    static const HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    std::cout.flush();
+    SetConsoleTextAttribute(hOut, colour);
+}
+void setcursor(int x, int y)//functiom  to set the position of cursor in console(0 indexing)
+{   // we need this function to set cursor and control where snake is
+    static const HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    std::cout.flush();
+    COORD coord = { (SHORT)x, (SHORT)y };
+    SetConsoleCursorPosition(hOut, coord);
+}
+
+void print_wall(){	//printing elments that do not change with time on screen
+  // printing score and difficulity
+	setcursor(0,0);
+	setConsoleColour(consoleforeground::DARKYELLOW);
+	cout<<"YOUR SCORE:                     ";
+	if(time1==20)		cout<<"HIGH";
+	else if(time1==50)	cout<<"MEDIUM";
+	else if(time1==80)	cout<<"LOW";
+	else 				{
+		cout<<"Custom: ";
+		setConsoleColour(consoleforeground::WHITE);
+		cout<<time1<<" ms";
+	}
+	cout<<"\n";
+	//printing walls
+	if(colour){
+		setConsoleColour(consolebackground::CYAN);
+		for(int i=0;i<lenght+2;i++)
+		cout<<" ";
+		cout<<"\n";
+		for(int i=0;i<width;i++){
+			for(int j=0;j<lenght+2;j++){
+				if(j==0  || j==lenght+1)	setConsoleColour(consolebackground::CYAN);
+				else	 					setConsoleColour(consolebackground::BLACK);
+				cout<<" ";
+			}
+			cout<<"\n";
+		}
+		setConsoleColour(consolebackground::CYAN);
+		for(int i=0;i<lenght+2;i++)
+		cout<<" ";
+	}
+}
+
+int main(){
+  
+}
